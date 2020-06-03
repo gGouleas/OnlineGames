75dada7776f7e865c8e7914c7e2c9ff3e88f1a5c
John G <gigouleas@gmail.com>
2020-06-03 20:52:50 +0300

    code improvements

--- src/main/webapp/resources/js/home-main.js
+++ src/main/webapp/resources/js/home-main.js
@@ -7,7 +7,7 @@ function showGames(field) {
     $('#gameImg').attr('alt', field.name);
     window.location.pathname === "/OnlineGames/home" ?
             $('#gameSrc').attr('href', 'home?unregistered') : $('#gameSrc')
-                    .attr('href', 'game-page?mainSrc='+field.mainSrc+"&title="+field.name);
+            .attr('href', 'game-page?mainSrc=' + field.mainSrc + "&title=" + field.name);
     $('#gameImg').tooltip();
     $(".selector").tooltip("classes.tooltip");
 }
@@ -22,24 +22,24 @@ function getAllGames() {
 
 function getCategoryFilter() {
     $.getJSON("category/", function (result) {
-        $('#categories').prepend('<option id="catOption" class="dropdown-item link"></option>');
-        $('#catOption').html('All');
-        $('#catOption').click(function () {
-            getAllGames();
-            $('#current-category').replaceWith('<span id="current-category">All</span>');
-        });
         $.each(result, function (i, field) {
             $('#categories').prepend('<option id="catOption" class="dropdown-item link"></option>');
             $('#catOption').html(field.name);
             $('#catOption').click(function () {
                 $('#games').empty();
-                $('#current-category').replaceWith('<span id="current-category">'+field.name+'</span>');
+                $('#current-category').replaceWith('<span id="current-category">' + field.name + '</span>');
                 $.getJSON("game/category/id/" + field.id, function (result) {
                     $.each(result, function (i, field) {
                         showGames(field);
                     });
                 });
             });
+        });
+        $('#categories').prepend('<option id="catOption" class="dropdown-item link"></option>');
+        $('#catOption').html('All');
+        $('#catOption').click(function () {
+            getAllGames();
+            $('#current-category').replaceWith('<span id="current-category">All</span>');
         });
     });
 }
